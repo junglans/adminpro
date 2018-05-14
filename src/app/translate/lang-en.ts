a541df6761e3723ce6679904904854104e3df0f6
jf.jimenez <jf.jimenez@mnemo.es>
2018-05-14 13:30:42 +0200

    Más traducciones

--- src/app/translate/lang-en.ts
+++ src/app/translate/lang-en.ts
@@ -3,4 +3,14 @@ export const LANG_EN_NAME = 'en';
 export const LANG_EN_TRANS =  {
     'placeholder.username': 'Username',
     'placeholder.password': 'Password',
+    'placeholder.confirm.password': 'Confirm password',
+    'text.dont.have.an.account': 'Don\'t you hava an account yet?',
+    'text.recover.password.1': 'Recover password',
+    'text.recover.password.2': 'Enter your Email and instructions will be sent to you!',
+    'text.remember.me': 'Remember me',
+    'text.forgot.password': 'Forgot your password?',
+    'text.agree.with': 'I agree with',
+    'text.terms': 'terms.',
+    'text.do.you.have.an.account': 'Do you have an account?',
+    'text.log.in': 'Log in'
 )
--- src/app/translate/lang-es.ts
+++ src/app/translate/lang-es.ts
@@ -3,5 +3,15 @@ export const LANG_ES_NAME = 'es';
 export const LANG_ES_TRANS =  {
     'placeholder.username' : 'Usuario',
     'placeholder.password' : 'Clave',
+    'placeholder.confirm.password': 'Confirmar clave',
+    'text.dont.have.an.account': '¿Todavía no tienes una cuenta?',
+    'text.recover.password.1': 'Recuperar clave',
+    'text.recover.password.2': '¡Introduce tu email y te enviaremos instrucciones!',
+    'text.remember.me': 'Recuérdame',
+    'text.forgot.password': '¿Olvidó la clave?',
+    'text.agree.with': 'Estoy de acuerdo con los',
+    'text.terms': 'términos.',
+    'text.do.you.have.an.account': '¿Dispones de una cuenta?',
+    'text.log.in': 'Ingresar'
 )
 
